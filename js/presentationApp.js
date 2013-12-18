3e7b28f5aee6a63e876416c969e1a36b7bf7e6c3
Derek Schindhelm <ddschindhelm@gmail.com>
2013-12-17 21:03:22 -0600

    Created clickable links that bring up presentation window
    . Also added keyboard controls in the index.html file as well as
    buttons.

--- js/presentationApp.js
+++ js/presentationApp.js
@@ -1,23 +1,49 @@
 var windowRef;
+
 $(function() {
-
-	windowRef = window.open("presentation.html",'titleTown','width=800, height=900');
-	windowRef.focus();
-
-    // Need to call on a delay to allow
-    // the child window to fully load...
-    window.setTimeout(callChildWindowFunction, 2500);
-	setTimeout(function(){
-			windowRef.postMessage("message","/presentation.html");
-		//window.frames[0].contentWindow.tester("test");
-	},2000);
 	
-	//  $(avgo.presentionWindow.document).load(function() {
-	 //   alert('loaded');
-	 //   do other things
-	//  });
+	$('body').keydown(function(e) {
+			console.log(e.which);
+			var dataToSend = {};
+			dataToSend.act = "changePlace";
+			switch (e.which) {
+				case 39 :
+					dataToSend.val = -1;
+					windowRef.postMessage(dataToSend,"*");
+					break;
+				case 37 :
+					dataToSend.val = 1;
+					windowRef.postMessage(dataToSend,"*");
+					break;
+				case 27 :
+					dataToSend.val = 1000;
+					windowRef.postMessage(dataToSend,"*");
+					break;
+			}
+	});
+
+	for (var eventName in avgo.eventData) 
+	{ 
+		$('#wrapper').append('<div>' + "<a href='javascript:linkClick(&#34;" + eventName + "&#34;)' id='link" + eventName + "' eventname='" + eventName + "'>" + eventName + '</a>' + '</div>');
+	}
+
+	$('.controls').hide().click(function() {
+		var dataToSend = {};
+		dataToSend.act = "changePlace";
+		dataToSend.val = parseInt($(this).attr('v'));
+		windowRef.postMessage(dataToSend,"*");
+	});
 });
 
-function callChildWindowFunction() {
-    //if ((windowRef != null) && (windowRef.closed == false)) windowRef.tester("test");
+function linkClick(data){
+	windowRef = window.open("presentation.html",'titleTown','width=800, height=900');
+	setTimeout(function() {
+		var dataToSend = {};
+		dataToSend.act = "setData";
+		dataToSend.array = avgo.eventData[data];
+		dataToSend.category = data;
+		windowRef.postMessage(dataToSend,"*");
+	}, 1000);
+	$('.controls').fadeIn(100);
+	
 }
